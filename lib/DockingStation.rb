e56185fe47018e99f919d94fb5fea111b059f8a4
savannaelbey <savanna_e@icloud.com>
2020-10-28 23:50:41 +0000

    adds unit test and method for #dock, adds an instance variable and an attribute reader

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,7 +1,17 @@
-
+require_relative 'Bike'
 
 class DockingStation
+
+  attr_reader :bike
+
   def release_bike
-    return Bike.new
+    Bike.new
   end
+
+  def dock(bike)
+    # use an instance varaible to store the bike in the state of this instance
+    @bike = bike
+  end
+
+
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,14 +1,18 @@
 require 'DockingStation'
 
 describe DockingStation do
-  it { should respond_to(:release_bike) }
 
-  describe '#release_bike' do
     it 'releases a working bike' do
       station = DockingStation.new
       bike = station.release_bike
       expect(bike).to be_a_kind_of(Bike)
       expect(bike.working?).to eq(true)
     end
-  end
+
+    it 'docks something' do
+      bike = Bike.new
+      # we want the dock method to return the bike we docked
+      expect(subject.dock(bike)).to eq bike
+    end
+    
 end
