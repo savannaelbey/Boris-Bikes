a80fbfaaac742772b820cc0f0fb9c38509b5b6f6
savannaelbey <savanna_e@icloud.com>
2020-10-29 17:58:46 +0000

    adds to #release_bike unit test and raises error

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -5,13 +5,13 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    Bike.new
+    fail 'No bikes available' unless @bike 
+    @bike
   end
 
   def dock(bike)
     # use an instance varaible to store the bike in the state of this instance
     @bike = bike
   end
-
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,16 +3,24 @@ require 'DockingStation'
 describe DockingStation do
 
     it 'releases a working bike' do
-      station = DockingStation.new
-      bike = station.release_bike
-      expect(bike).to be_a_kind_of(Bike)
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
       expect(bike.working?).to eq(true)
     end
+
 
     it 'docks something' do
       bike = Bike.new
       # we want the dock method to return the bike we docked
       expect(subject.dock(bike)).to eq bike
     end
-    
+
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      # Again, we need to return the bike we just docked
+      expect(subject.bike).to eq bike
+    end
+
 end
