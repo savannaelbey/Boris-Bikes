7f13469a915aad35ae14f028cf7692673cd3de38
savannaelbey <savanna_e@icloud.com>
2020-10-30 00:33:40 +0000

    added private methods (full? and empty?) to refactor code

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -9,14 +9,24 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
     # use an instance varaible to store the bike in the state of this instance
-    fail 'Docking station full!' if @bikes.count >= 20
+    fail 'Docking station full!' if full?
     @bikes.push(bike)
+  end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
